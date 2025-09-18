0a132effe65505bd72c09c79612086f8f24ee1c0
TheFifthRider <flare777@gmail.com>
2025-09-18 11:30:42 -0400

    build: Downgrade grafana

--- OpenTelemetryAgent/OpenTelemetryAgentModSystem.cs
+++ OpenTelemetryAgent/OpenTelemetryAgentModSystem.cs
@@ -1,11 +1,12 @@
 ﻿using Vintagestory.API.Client;
 using Vintagestory.API.Server;
-using Vintagestory.API.Config;
 using Vintagestory.API.Common;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 using Grafana.OpenTelemetry;
+using System;
+using System.Diagnostics.Metrics;
 
 namespace OpenTelemetryAgent;
 
@@ -18,6 +19,7 @@ public class OpenTelemetryAgentModSystem : ModSystem
     {
         Environment.SetEnvironmentVariable("OTEL_RESOURCE_ATTRIBUTES", "service.name=local-vs-server,service.namespace=local-vs-namespace,deployment.environment=production");
         Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT", "https://otlp-gateway-prod-us-east-2.grafana.net/otlp");
+        Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_HEADERS", "Authorization=Basic MTM3NTk3OTpnbGNfZXlKdklqb2lNVFV6TkRJek9DSXNJbTRpT2lKdmRHVnNZV2RsYm5RdGRHOXJaVzRpTENKcklqb2lUV0V6TXpsMGVqWTBhak0xYjJOT09XaEZZVFI2U2poRklpd2liU0k2ZXlKeUlqb2ljSEp2WkMxMWN5MWxZWE4wTFRBaWZYMD0=");
         Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf");
         using var tracerProvider = Sdk.CreateTracerProviderBuilder()
             .UseGrafana()
